8514e6604e400977dbb8e30526f720da8f7f6f47
DayanMonteiro <dmsmamb@gmail.com>
2021-05-22 00:32:18 -0300

    esboso inicial

--- marvel-comics-store-app/src/app/app.module.ts
+++ marvel-comics-store-app/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SectionCardComponent } from './components/section-card/section-card.component';
+import { OfertasListaComponent } from './components/ofertas/ofertas-lista/ofertas-lista.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    NavbarComponent,
+    SectionCardComponent,
+    OfertasListaComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
